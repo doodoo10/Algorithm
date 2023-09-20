6b75c147e9c3c151ce106fdfde1e63562444596d
정여진 <145098307+doodoo10@users.noreply.github.com>
2023-09-20 23:42:11 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
@@ -1,12 +1,10 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int main() {
-	int d, n, h;
-	int day = 0;
-	scanf("%d %d %d", &d, &n, &h);
-
-	day = (h - d + (d - n) - 1) / (d - n) + 1;
-	printf("%d", day);
-	return 0;
+int main(void){
+    int a, b, v;
+    int day;
+    scanf("%d %d %d", &a, &b, &v);
+    day = ( v - b -1) / (a - b)+1;
+    printf("%d", day);
+    return 0;
 }
